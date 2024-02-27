878b6c1978114dd2c12a8c5a4ae134a57d677e10
Rohadgal <83172284+Rohadgal@users.noreply.github.com>
2024-02-27 09:01:44 -0600

    Started Layer script

---
+++ Assets/Scenes/Layer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer : MonoBehaviour
+{
+    Neuron[] neuron;
+    float[] weights;
+    float[] bias;
+    
+    public float[] getOutput()
+    {
+        float[] results = new float[neuron.Length];
+        
+        for(int i = 0; i < neuron.Length; i++)
+        {
+           
+
+        }
+        return results;
+    }
+}
